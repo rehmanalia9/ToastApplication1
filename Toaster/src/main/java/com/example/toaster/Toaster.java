1e9fced2c8fc732c90f160745951ce077fda7895
GENIOUS <rehmanalia9@gmail.com>
2021-12-10 11:30:56 +0500

    final commit

--- Toaster/src/main/java/com/example/toaster/Toaster.java
+++ Toaster/src/main/java/com/example/toaster/Toaster.java
@@ -1,4 +1,12 @@
 package com.example.toaster;
 
+import android.content.Context;
+import android.widget.Toast;
+
 public class Toaster {
+
+    public static void SimpleToast(Context context,String toast){
+
+        Toast.makeText(context,toast, Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/example/toastapplication/MainActivity.java
+++ app/src/main/java/com/example/toastapplication/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.toaster.Toaster;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toaster.SimpleToast(this,"Hello genious");
     }
 }
